b26d8af25df8d8f74827025cff3a25c2b63975cb
teddylopez <tedmlopez@gmail.com>
2020-12-23 12:16:12 -0500

    Implement seller view

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -1,16 +1,22 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
-import { isAdmin, isAuth } from "../utils.js";
+import { isAdmin, isAuth, isSellerOrAdmin } from "../utils.js";
 
 const orderRouter = express.Router();
 
 orderRouter.get(
   "/",
   isAuth,
-  isAdmin,
+  isSellerOrAdmin,
   expressAsyncHandler(async (req, res) => {
-    const orders = await Order.find({}).populate("user", "name");
+    const seller = req.query.seller || "";
+    const sellerFilter = seller ? { seller } : {};
+
+    const orders = await Order.find({ ...sellerFilter }).populate(
+      "user",
+      "name"
+    );
     res.send(orders);
   })
 );
@@ -40,6 +46,7 @@ orderRouter.post(
         taxPrice: req.body.taxPrice,
         totalPrice: req.body.totalPrice,
         user: req.user._id,
+        seller: req.body.orderItems[0].seller,
       });
       const createdOrder = await order.save();
       res
